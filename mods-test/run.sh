e3549ab493908e12a2496b88013689a11a1b75e3
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-01-15 17:11:39 -0800

    Turn into a test case.

--- mods-test/run.sh
+++ mods-test/run.sh
@@ -1,7 +1,20 @@
-rm -f nginx.error.log apache.error.log apache2.pid
+rm -f apache.error.log apache2.pid
+cat > apache2.conf apache2.base.conf - <<EOF
+Redirect / https://eff.org/
+ProxyPass "/" "http://google.com/"
+RewriteEngine on
+RewriteRule "/.well-known/acme-challenge/(.*)" "/\$1" [END]
+EOF
+
+echo "good response" > 1234
+
 apache2 -d . -k start -X &
 sleep 0.2
-curl -iL http://localhost:8800/index.html
-curl -iL http://localhost:8800/.well-known/acme-challenge/1234
+
+RESPONSE=$(curl -sL http://localhost:8800/.well-known/acme-challenge/1234 --max-redirs 5)
+if [[ $RESPONSE != "good response" ]] ; then
+	echo "bad response"
+fi
+
 tail apache.error.log
 killall apache2
