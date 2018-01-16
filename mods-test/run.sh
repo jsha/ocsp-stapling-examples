f26bfa8f7ee0a9b196543a04ed8b571dd6bcb5ec
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-01-15 17:55:11 -0800

    Add more test cases.

--- mods-test/run.sh
+++ mods-test/run.sh
@@ -1,20 +1,37 @@
-rm -f apache.error.log apache2.pid
+echo "good response" > 1234
+
+checkit() {
+  rm -f apache.error.log apache2.pid
+  apache2 -d . -k start -X &
+  sleep 0.2
+  RESPONSE=$(curl -sL http://localhost:8800/.well-known/acme-challenge/1234 --max-redirs 5)
+  if [[ $RESPONSE != "good response" ]] ; then
+    echo "bad response"
+  fi
+  tail apache.error.log
+  killall apache2
+}
+
 cat > apache2.conf apache2.base.conf - <<EOF
 Redirect / https://eff.org/
 ProxyPass "/" "http://google.com/"
 RewriteEngine on
 RewriteRule "/.well-known/acme-challenge/(.*)" "/\$1" [END]
 EOF
+checkit
 
-echo "good response" > 1234
+cat > apache2.conf apache2.base.conf - <<EOF
+RewriteEngine on
+RewriteRule "/.well-known/acme-challenge/(.*)" "/\$1" [END]
+Redirect / https://eff.org/
+ProxyPass "/" "http://google.com/"
+EOF
+checkit
 
-apache2 -d . -k start -X &
-sleep 0.2
-
-RESPONSE=$(curl -sL http://localhost:8800/.well-known/acme-challenge/1234 --max-redirs 5)
-if [[ $RESPONSE != "good response" ]] ; then
-	echo "bad response"
-fi
-
-tail apache.error.log
-killall apache2
+cat > apache2.conf apache2.base.conf - <<EOF
+RewriteEngine on
+Redirect / https://eff.org/
+RewriteRule "/.well-known/acme-challenge/(.*)" "/\$1" [END]
+ProxyPass "/" "http://google.com/"
+EOF
+checkit
