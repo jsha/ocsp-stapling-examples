1151c8715de14c976deadac790eb1655f9903611
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-01-15 16:43:53 -0800

    Fix authz issues.

--- mods-test/run.sh
+++ mods-test/run.sh
@@ -1,1 +1,6 @@
-apache2 -d . -k start -X
+rm -f nginx.error.log apache.error.log apache2.pid
+apache2 -d . -k start -X &
+sleep 0.2
+curl http://localhost:8800/
+tail apache.error.log
+killall apache2
