8ed81c7ed2215447b829ce41b7f049cf9f377866
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-01-15 16:45:45 -0800

    Add details.

--- mods-test/run.sh
+++ mods-test/run.sh
@@ -1,6 +1,7 @@
 rm -f nginx.error.log apache.error.log apache2.pid
 apache2 -d . -k start -X &
 sleep 0.2
-curl http://localhost:8800/
+curl -iL http://localhost:8800/index.html
+curl -iL http://localhost:8800/.well-known/acme-challenge/1234
 tail apache.error.log
 killall apache2
